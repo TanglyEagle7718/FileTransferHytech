a8f09cdb247125ed3f751bed113854e163471423
Rishi B <74271243+TanglyEagle7718@users.noreply.github.com>
2024-03-14 18:18:41 -0400

    Create fromCarToPanda.sh

---
+++ car/fromCarToPanda.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+cd recordings
+mkdir filesToBeSent
+cd ..
+
+if [ -f dataTransferMessage.txt ];
+then
+	i=1	
+else
+	mv -v ./recordings/recordings/* ./recordings/filesToBeSent/
+fi
+
+tar -cf recordings.tar ./recordings/filesToBeSent/
+
+sha256sum recordings.tar | awk '{print $1}'  > sha256Recordings.txt
+
+tar -cf finalDataPackage.tar recordings.tar sha256Recordings.txt
+
+rsync -au finalDataPackage.tar practicefedora@[IP address]:~/carData
